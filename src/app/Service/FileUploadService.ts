aab12c62aa7cc6f6a331072dbbafcbd4c5c8bcc9
tausifabir <tausifabir94@gmail.com>
2022-04-21 05:00:53 +0600

    Refactoring backend api

--- src/app/Service/FileUploadService.ts
+++ src/app/Service/FileUploadService.ts
@@ -1,7 +1,8 @@
 import {HttpClient, HttpEvent, HttpErrorResponse, HttpEventType, HttpRequest} from '@angular/common/http';
-import { map } from  'rxjs/operators';
+import {map} from 'rxjs/operators';
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +11,8 @@ export class FileUploadService {
   SERVER_URL: string = "https://file.io/";
   resourceUrl: string = "http://localhost:8080/api/employee-mgt/employee-static-files/id-card";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   public upload(formData: FormData) {
 
@@ -31,9 +33,70 @@ export class FileUploadService {
       responseType: 'json',
     });*/
 
-    this.httpClient.post(this.resourceUrl,formData)
+    this.httpClient.post(this.resourceUrl, formData)
       .subscribe(
-      (res)=>{console.log(res);}
-    );
+        (res) => {
+          console.log(res);
+        }
+      );
   }
+
+
+  /*  @PostMapping("/employee-static-files/id-card")
+    public ResponseEntity<Boolean> createEmployeeStaticFileIdCard(@RequestParam("file") MultipartFile[] files) throws URISyntaxException {
+
+    log.debug("REST request to save id-card : {}", files);
+    Boolean isSaved = employeeStaticFileService.getEmployeeStaticFileDTO(files);
+    return ResponseEntity.ok(isSaved);
+  }*/
+
+
+  /*@Override
+  public Boolean getEmployeeStaticFileDTO(MultipartFile[] files) {
+
+  Boolean isSavedFile = Boolean.FALSE;
+
+  if (files.length > 0) {
+  for (MultipartFile multipartFile : files) {
+  if (multipartFile.getContentType().equals("image/jpeg") ||
+  multipartFile.getContentType().equals("image/apng") ||
+  multipartFile.getContentType().equals("image/avif") ||
+  multipartFile.getContentType().equals("image/gif") ||
+  multipartFile.getContentType().equals("image/jpg") ||
+  multipartFile.getContentType().equals("image/jfif") ||
+  multipartFile.getContentType().equals("image/pjpeg") ||
+  multipartFile.getContentType().equals("image/pjp") ||
+  multipartFile.getContentType().equals("image/png") ||
+  multipartFile.getContentType().equals("image/svg") ||
+  multipartFile.getContentType().equals("image/webp")
+) {
+  String fileNameWithOutExtension = multipartFile.getOriginalFilename()
+    .substring(0, multipartFile.getOriginalFilename().indexOf("."));
+
+  Optional<Employee> employee = employeeRepository.findByPin(fileNameWithOutExtension);
+  EmployeeStaticFile employeeStaticFile = new EmployeeStaticFile();
+
+  if (employee.isPresent()) {
+
+  try {
+  File savedFile = employeeIdCardService.save(multipartFile);
+  employeeStaticFile.setEmployee(employee.get());
+  //TODO: query for EmployeeStaticFile
+  /!* if(!employeeStaticFile.getFilePath().isEmpty()){
+
+       fileOperationService.delete(employeeStaticFile.getFilePath());
+   }*!/
+  employeeStaticFile.setFilePath(savedFile.getAbsolutePath());
+  employeeStaticFileRepository.save(employeeStaticFile);
+  isSavedFile = Boolean.TRUE;
+} catch (Exception e) {
+  throw new RuntimeException(e);
+}
+
+}
+}
+}
+}
+return isSavedFile;
+}*/
 }
